bdcd946145f26079a70e1af305ead7b5ecf90c88
daviswww <ho861206@gmail.com>
2020-10-18 12:35:33 +0800

    firestoredb

--- models/FirestoreOfSql.rb
+++ models/FirestoreOfSql.rb
@@ -1,33 +1,29 @@
-require "./include/FirestoreOfDbh"
+require './include/FirestoreOfDbh'
 
 class FirestoreSql < FirestoreDbh
-  @table = ''
-  @name = ''
-  @data = ''
-  
   def initialize
     @firestore = FirestoreDbh.new.connect
+  end
+
+  private
+
+  def set(table, name)
+    @table = table
+    @name = name
   end
 
   public
 
   def insert(table, name, data)
-    @table = table
-    @name = name
+    set(table, name)
     @data = data
     ref = @firestore.doc "#{@table}/#{@name}"
     ref.set(@data)
   end
 
   def select(table, name)
-    @table = table
-    @name = name
+    set(table, name)
     ref = @firestore.doc "#{@table}/#{@name}"
     ref.get
   end
 end
-
-# db = FirestoreSql.new
-# datas = db.select('asd', 'qwe')
-# puts datas[:qwe]
-# db.insert('asd', 'qwe', {sad: 'ggg', qwe: 'ssd'})
